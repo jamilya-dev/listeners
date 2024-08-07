8ba6e9e4d9348725f62fd34e936a25a33a593384
jamilya-dev <omg.groups@yandex.ru>
2024-08-07 17:16:00 +0300

    выполнено задание 11

--- script.js
+++ script.js
@@ -1,4 +1,30 @@
+'use strict';
 
+const btn = document.getElementById('btn');
+const e_btn = document.getElementById('e_btn');
+const square = document.getElementById('square');
+const input = document.getElementById('text');
+const circle = document.getElementById('circle');
+const range = document.getElementById('range');
+let color = '';
+let size = '';
+
+const squareColor = function () {
+  square.style.backgroundColor = color;
+  e_btn.style.display = "none";
+}
+
+input.addEventListener('change', function (event) {
+  color = event.target.value;
+})
+
+btn.addEventListener('click', squareColor);
+
+range.addEventListener('input', function (event) {
+  size = event.target.value;
+  square.style.height = size + '%';
+  square.style.width = size + '%';
+});
 
 
 
